0f989bd203f59ef730699b96b6edf7e4fe3dcb8b
Yassafa <12302332@q10422.iutv.univ-paris13.fr>
2025-05-02 12:28:03 +0200

    ajout setup dans VoitureTest

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -8,6 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class VoitureTest {
+    private Voiture car;
+
+    @BeforeEach
+    void setup() {
+        this.car = new Voiture("Porshe", 9999999);
+    }
 
     @Test
     void creerVoiture(){
