060bfdcfe23995e157561e94c410bdeb128c1083
sherwin.jeremias@edu.univ-paris13.fr <12300478@q10425.iutv.univ-paris13.fr>
2025-05-09 11:12:05 +0200

    Update build.gradle

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
@@ -20,4 +21,5 @@ public class VoitureTest {
         assertEquals(1,1);
     }
 
+    //test ssh
 }
